435a250e40d89d2bbf9992c2fcf60ad943a17000
wuhuanqing <18610496940@qq.com>
2016-09-20 11:59:59 +0800

    url

--- app/src/main/java/com/qf/administrator/xiongmao/ui/fragments/AmusementFragment.java
+++ app/src/main/java/com/qf/administrator/xiongmao/ui/fragments/AmusementFragment.java
@@ -19,6 +19,7 @@ public class AmusementFragment extends BaseFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         layout = inflater.inflate(R.layout.fragment_amusement,container,false);
+        //娱乐
         return layout;
     }
 }
